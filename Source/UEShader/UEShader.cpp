22197e580c03ccfc04211dc6a65e55937e52192d
v0tx <wellingfine@qq.com>
2025-01-20 23:13:42 +0800

    新建空白c++工程

---
+++ Source/UEShader/UEShader.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UEShader.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UEShader, "UEShader" );
