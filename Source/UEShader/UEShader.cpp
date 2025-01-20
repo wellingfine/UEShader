c90d664ab7e904d1fb2c2f1edeb15c85565782a0
v0tx <wellingfine@qq.com>
2025-01-20 23:33:20 +0800

    fix 加载模块

--- Source/UEShader/UEShader.cpp
+++ Source/UEShader/UEShader.cpp
@@ -14,4 +14,6 @@ void FShaderLabModule::ShutdownModule(){
 }
 
 IMPLEMENT_PRIMARY_GAME_MODULE(FShaderLabModule, UEShader, "UEShader");
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UEShader, "UEShader" );
+
+// 默认的要注释掉，不然会出现【重定义多次初始化】
+// IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UEShader, "UEShader" );
