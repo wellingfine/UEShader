015bca9d1b6dc0a5634fa139f5d205479ebd45a0
v0tx <wellingfine@qq.com>
2025-12-07 21:02:03 +0800

    升级5.7.1

--- Source/UEShader.Target.cs
+++ Source/UEShader.Target.cs
@@ -5,11 +5,11 @@ using System.Collections.Generic;
 
 public class UEShaderTarget : TargetRules
 {
-	public UEShaderTarget(TargetInfo Target) : base(Target)
-	{
-		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
-		ExtraModuleNames.Add("UEShader");
-	}
+  public UEShaderTarget(TargetInfo Target) : base(Target)
+  {
+    Type = TargetType.Game;
+    DefaultBuildSettings = BuildSettingsVersion.V6;
+    IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
+    ExtraModuleNames.Add("UEShader");
+  }
 }
